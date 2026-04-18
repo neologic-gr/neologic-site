44b6e7daa6cec442d896654d4e10ded1ce0d229e
neologic-gr <eleftherios.p.eleftheriou@gmail.com>
2026-04-18 22:25:22 +0300

    Update calculate.js

--- netlify/functions/calculate.js
+++ netlify/functions/calculate.js
@@ -1,1 +1,9 @@
+exports.handler = async (event) => {
+  const { value } = JSON.parse(event.body);
+  const result = value * 0.0085;
 
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ result })
+  };
+};
